aae3a632902300d035dce323b6a9c33da155f5d5
midhun kumar <midh0001@student.monash.edu>
2026-01-01 20:00:47 +1100

    chrore: incorrect build of tauri

--- pomodoro/src-tauri/src/main.rs
+++
@@ -1,9 +0,0 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    // NOTE: Currently only using the rust backend just to render the frontend, prolly add a sqlitedb or remote api later
-    tauri::Builder::default()
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
